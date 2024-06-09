1b5fa0d359998e040853fb4ccb2aa7f40675dc1d
macs1756 <kindrativmaksum312@gmail.com>
2024-06-09 22:14:30 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PRIMARY_TOKEN: process.env.PRIMARY_TOKEN,
+    DOMAIN_FRONTEND: process.env.DOMAIN_FRONTEND,
+    DOMAIN_BACKEND: process.env.DOMAIN_BACKEND,
+  }
 };
 
 export default nextConfig;

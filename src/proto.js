1899d869fc3d018ecb46edf67d698821ba0e4654
macs1756 <kindrativmaksum312@gmail.com>
2024-06-26 22:52:09 +0300

    proto final

--- src/proto.js
+++ src/proto.js
@@ -8,15 +8,31 @@ Array.prototype.log = function () {
   console.log(this);
 }
 
+Number.prototype.log = function () {
+  console.log(this);
+}
+
+
 String.prototype.toFirstUpper = function () {
   return this[0].toUpperCase() + this.substring(1)
 }
 
-const arr = [1, 2, 3, 4];
+Array.prototype.clearDoubles = function () {
+  return this.filter((item, index) => this.indexOf(item) === index);
+}
+
+
+Number.prototype.toDouble = function () {
+  return (this * 2).log()
+}
+
+const arr = [1, 2, 2, 3, 4, 4];
 const firstLine = 'Just line';
 const lowerText = 'maksim'
 
 firstLine.log()
 arr.log()
 lowerText.toFirstUpper().log()
+arr.clearDoubles().log()
 
+arr[2].toDouble()
